a27ea17f5626a1ace4a3db811cfd9ce773003452
Ceren04 <e230503059@stud.tau.edu.tr>
2025-08-22 15:36:58 +0300

    walk flip

--- player.gd
+++ player.gd
@@ -20,14 +20,24 @@ func _handle_input() -> Vector2:
 		velocity.x -= 1
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+				
+			
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play("walk", 2.0)	
 	else : 
 		$AnimatedSprite2D.play("idle")
-		
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
 	return velocity
 func _process(delta: float) -> void:
-	
 	position += delta * _handle_input()
+	
+
+		
 	
