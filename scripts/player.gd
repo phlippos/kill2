c323c173eef543b6b6627bbb14d2b8804f5aa66e
Ceren04 <e230503059@stud.tau.edu.tr>
2025-09-01 14:33:36 +0300

    shot

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,7 +11,8 @@ var hp: int = 100
 var player_id: int
 var status: bool = true
 var jump_count: int = 0 
-
+var is_shooting: bool = false
+var shot_timer: Timer  
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -19,28 +20,36 @@ func _ready() -> void:
 	$AnimatedSprite2D.play("idle")
 	health_bar._setup_health_bar(100.0, true)
 	
+	shot_timer = Timer.new()
+	shot_timer.wait_time = 0.5
+	shot_timer.one_shot = true
+	shot_timer.timeout.connect(_on_shot_timer_timeout)
+	add_child(shot_timer)
+
 func handle_input() -> void:
 	var dir_x := 0
+	
+	if Input.is_action_just_pressed("shot") and not is_shooting:
+		$AnimatedSprite2D.play("shot")
+		is_shooting = true
+		shot_timer.start()  
 	
 	if Input.is_action_pressed("move_left"):
 		dir_x -= 1
 	if Input.is_action_pressed("move_right"):
 		dir_x += 1
-		
-	if Input.is_action_just_pressed("shot"):
-		$AnimatedSprite2D.play("shot")
 	
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
-
 			velocity.y = -jump_force
 			jump_count = 0
-			$AnimatedSprite2D.play("jump")
+			if not is_shooting:
+				$AnimatedSprite2D.play("jump")
 		elif jump_count < max_jumps - 1:
-
 			velocity.y = -jump_force
 			jump_count += 1
-			$AnimatedSprite2D.play("jump")
+			if not is_shooting:
+				$AnimatedSprite2D.play("jump")
 	
 	if is_on_floor():
 		jump_count = 0
@@ -49,14 +58,18 @@ func handle_input() -> void:
 	if dir_x != 0:
 		$AnimatedSprite2D.flip_h = dir_x < 0
 	
-	if is_on_floor():
-		if dir_x != 0:
-			$AnimatedSprite2D.play("walk", 2.0)
+	if not is_shooting:
+		if is_on_floor():
+			if dir_x != 0:
+				$AnimatedSprite2D.play("walk", 2.0)
+			else:
+				$AnimatedSprite2D.play("idle")
 		else:
-			$AnimatedSprite2D.play("idle")
-	else:
-		if $AnimatedSprite2D.animation != "jump":
-			$AnimatedSprite2D.play("jump")
+			if $AnimatedSprite2D.animation != "jump":
+				$AnimatedSprite2D.play("jump")
+
+func _on_shot_timer_timeout() -> void:
+	is_shooting = false
 
 func take_damage(amount: int) -> void:
 	hp -= amount
@@ -73,6 +86,7 @@ func respawn(new_position: Vector2) -> void:
 		hp = 100
 		status = true
 		jump_count = 0  
+		is_shooting = false
 
 func fire() -> void:
 	pass
@@ -85,6 +99,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 	handle_input()
 	move_and_slide()
+
 func set_username(username: String) -> void:
 	self.username = username
 	$UserNameLabel.text = self.username
